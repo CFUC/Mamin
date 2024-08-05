7b24095250560bcdaa2a64b26035bf22b115aaf3
choipaco <kfckim1014@naver.com>
2024-08-05 17:53:59 +0900

    Feat: navbar update

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -19,44 +19,51 @@ export default function Navbar(){
         }
     },[scroll])
     return(
-        <div className={styles.main}>
+        <div className={styles.main}
+        style={{
+            background: `${black ? '#FFFFFF' : 'none'}`,
+            borderBottom: `${ black ? "1px solid #DDDDDD" : "none"}`
+    }}
+        >
+           <div className={styles.mainContainer}>
             <Link
-                to='1'
-                spy={true}
-                smooth={true}
-                className={!black ? styles.logo : styles.logoBlack}
-                >
-            </Link>
-            <div className={styles.listContainer}>
-                <Link
-                to='2'
-                spy={true}
-                smooth={true}
-                >
-                    <label>회사소개</label>
+                    to='1'
+                    spy={true}
+                    smooth={true}
+                    className={!black ? styles.logo : styles.logoBlack}
+                    >
                 </Link>
-                <Link
-                to='3'
-                spy={true}
-                smooth={true}
-                >
-                    <label>서비스</label>
-                </Link>
-                <Link
-                to='4'
-                spy={true}
-                smooth={true}
-                >
-                    <label>포트폴리오</label>
-                </Link>
-                <Link
-                to='5'
-                spy={true}
-                smooth={true}
-                >
-                    <label>광고문의</label>
-                </Link>
-            </div>
+                <div className={styles.listContainer}>
+                    <Link
+                    to='2'
+                    spy={true}
+                    smooth={true}
+                    >
+                        <label>회사소개</label>
+                    </Link>
+                    <Link
+                    to='3'
+                    spy={true}
+                    smooth={true}
+                    >
+                        <label>서비스</label>
+                    </Link>
+                    <Link
+                    to='4'
+                    spy={true}
+                    smooth={true}
+                    >
+                        <label>포트폴리오</label>
+                    </Link>
+                    <Link
+                    to='5'
+                    spy={true}
+                    smooth={true}
+                    >
+                        <label>광고문의</label>
+                    </Link>
+                </div>
+           </div>
         </div>
     )
 }
