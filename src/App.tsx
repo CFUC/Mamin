d3e05aa038b751393f0a7e9fb38262895551d277
wjdgotjd <wjdgotjd529@naver.com>
2024-08-05 16:02:13 +0900

    서비스소개, 포트폴리오, 광고사 리스트, 광고절차 컴포넌트 App.tsx 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import AdList from "./components/adlist/AdList";
+import AdProcess from "./components/adprocess/AdProcess";
+import Portfolio from "./components/portfolio/Portfolio";
+import ServiceIntroduce from "./components/serviceIntroduce/ServiceIntroduce";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <ServiceIntroduce />
+      <Portfolio />
+      <AdList />
+      <AdProcess />
+    </>
+  );
 }
 
 export default App;
