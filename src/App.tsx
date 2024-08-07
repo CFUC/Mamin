fbed6cb0aaf97905e46930ea0f886adb114d3868
gy1232 <xguoying0822@gmail.com>
2024-08-07 11:21:36 +0900

    주석 복구

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import "./App.css";
-import AdList from "./components/adlist/AdList";
-import AdProcess from "./components/adprocess/AdProcess";
-import Portfolio from "./components/portfolio/Portfolio";
-import ServiceIntroduce from "./components/serviceIntroduce/ServiceIntroduce";
-import Introduce from "./components/introduce/introduce";
-import Main from "./components/main/main";
-import Navbar from "./components/navbar/navbar";
-import Services from "./components/services/services";
-import Inquliry from "./components/inquliry/Inquliry";
-import MapCom from "./components/map/MapCom";
-import Footer from "./components/footer/Footer";
-import MetaTag from "./common/MetaTag";
+import './App.css';
+import AdList from './components/adlist/AdList';
+import AdProcess from './components/adprocess/AdProcess';
+import Portfolio from './components/portfolio/Portfolio';
+import ServiceIntroduce from './components/serviceIntroduce/ServiceIntroduce';
+import Introduce from './components/introduce/introduce';
+import Main from './components/main/main';
+import Navbar from './components/navbar/navbar';
+import Services from './components/services/services';
+import Inquliry from './components/inquliry/Inquliry';
+import MapCom from './components/map/MapCom';
+import Footer from './components/footer/Footer';
+import MetaTag from './common/MetaTag';
 
 function App() {
   return (
@@ -25,7 +25,7 @@ function App() {
       <AdList />
       <AdProcess />
       <Inquliry />
-      {/* <MapCom /> */}
+      <MapCom />
       <Footer />
     </>
   );
