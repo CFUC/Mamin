5be6a2ee6cd7b2d37f0f465e3f992ab5bdc4335f
wjdgotjd <wjdgotjd529@naver.com>
2024-08-08 14:31:23 +0900

    코드 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
 import "./App.css";
-import AdList from "./components/adlist/AdList";
-import AdProcess from "./components/adprocess/AdProcess";
-import Portfolio from "./components/portfolio/Portfolio";
-import ServiceIntroduce from "./components/serviceIntroduce/ServiceIntroduce";
-import Introduce from "./components/introduce/introduce";
-import Main from "./components/main/main";
 import Navbar from "./components/navbar/navbar";
-import Services from "./components/services/services";
-import Inquliry from "./components/inquliry/Inquliry";
-import MapCom from "./components/map/MapCom";
 import Footer from "./components/footer/Footer";
 import MetaTag from "./common/MetaTag";
 import { Route, Routes } from "react-router-dom";
