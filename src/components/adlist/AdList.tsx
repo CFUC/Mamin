146dd9702f467590db3e4f71176bf3ae3f015461
wjdgotjd <wjdgotjd529@naver.com>
2024-08-12 09:31:00 +0900

    광고사 리스트 이미지 순서 변경 및 크기 변경

--- src/components/adlist/AdList.tsx
+++ src/components/adlist/AdList.tsx
@@ -7,52 +7,52 @@ import { Autoplay } from "swiper/modules";
 const adData = [
   {
     id: 1,
-    title: "banksalad",
+    title: "세이브더칠드런",
     image: "images/ad1.png",
   },
   {
     id: 2,
-    title: "uniqlo",
+    title: "초록우산 어린이재단",
     image: "images/ad2.png",
   },
   {
     id: 3,
-    title: "casamia",
+    title: "밀알복지재단",
     image: "images/ad3.png",
   },
   {
     id: 4,
-    title: "woowahan",
+    title: "대구대학교",
     image: "images/ad4.png",
   },
   {
     id: 5,
-    title: "hyundai",
+    title: "경북소프트웨어마이스터고등학교",
     image: "images/ad5.png",
   },
   {
     id: 6,
-    title: "elandmall",
+    title: "경북바이오마이스터고등학교",
     image: "images/ad6.png",
   },
   {
     id: 7,
-    title: "moel",
+    title: "susoft",
     image: "images/ad7.png",
   },
   {
     id: 8,
-    title: "moel",
+    title: "phase",
     image: "images/ad8.png",
   },
   {
     id: 9,
-    title: "moel",
+    title: "rocketry",
     image: "images/ad9.png",
   },
   {
     id: 10,
-    title: "moel",
+    title: "klapoo",
     image: "images/ad10.png",
   },
 ];
