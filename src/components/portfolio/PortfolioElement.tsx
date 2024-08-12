1967a438030f3ed56187973ec7840ca5493c14c6
wjdgotjd <wjdgotjd529@naver.com>
2024-08-12 09:31:36 +0900

    포트폴리오 일반&호버 이미지 변경 및 테두리 색 변경

--- src/components/portfolio/PortfolioElement.tsx
+++ src/components/portfolio/PortfolioElement.tsx
@@ -3,27 +3,22 @@ import styles from "./PortfolioElement.module.css";
 const PortfolioElement = ({
   image,
   index,
-  adType,
-  customer,
+  hover,
 }: {
   image: string;
   index: number;
-  adType: string;
-  customer: string;
+  hover: string;
 }) => {
   return (
     <div
       className={styles.container}
       data-aos="fade-up"
-      data-aos-delay={100 * (index - 1)}
-      style={{
-        backgroundImage: `url("${image}")`,
-      }}
+      data-aos-delay={50 * (index - 1)}
     >
       <div className={styles.hoverDiv}>
-        <div className={styles.adType}>{adType}</div>
-        <div className={styles.customer}>{customer}</div>
+        <img src={hover} className={styles.hoverImage} />
       </div>
+      <img src={image} className={styles.image} />
     </div>
   );
 };
