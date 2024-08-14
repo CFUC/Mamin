9a45774c27b6a6593ace5395a0950da4cc9ffa38
wjdgotjd <wjdgotjd529@naver.com>
2024-08-14 17:30:03 +0900

    배포 URL 변경

--- src/common/MetaTag.tsx
+++ src/common/MetaTag.tsx
@@ -1,15 +1,12 @@
 import { Helmet } from "react-helmet-async";
 
-
-
-const DEPLOY_URL = "http://localhost:3000";
+const DEPLOY_URL = "https://마케팅민족.com";
 
 export default function MetaTag() {
-
   return (
     <Helmet>
       <title>마케팅 민족</title>
-      <meta name="description" content={"마케팅의 모든 것"}/>
+      <meta name="description" content={"마케팅의 모든 것"} />
       <meta property="og:type" content="website" />
       <meta property="og:title" content={"마케팅 민족"} />
       <meta property="og:site_name" content="마케팅 민족" />
